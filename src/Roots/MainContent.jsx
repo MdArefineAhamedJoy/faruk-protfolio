62193daa7f90b6c6618b7d408d2e0cc96b4165be
Md Arefine Ahamed Joy <mdarefine05@gmail.com>
2023-06-30 23:21:20 +0600

    add osame navbar

---
+++ src/Roots/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../Components/Navbar';
+
+const MainContent = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default MainContent;
